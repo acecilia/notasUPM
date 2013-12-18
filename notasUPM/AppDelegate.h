e399084aa9dd39d284bed206fc3f87e1a886dd0f
Alvaro Roman <alvaroromanbengoechea@w220068.ccupm.upm.es>
2013-12-18 20:46:03 +0100

    Initial Commit

---
+++ notasUPM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  notasUPM
+//
+//  Created by Alvaro Roman on 18/12/13.
+//  Copyright (c) 2013 Alvaro Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
