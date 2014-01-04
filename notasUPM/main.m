379587f8780a6a30a8eb095a24e84e47eb3e0670
acecilia <a.cecilia.luque@gmail.com>
2014-01-04 02:17:10 +0100

    No me gira puta mierda..snif

--- notasUPM/main.m
+++ notasUPM/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  notasUPM
-//
-//  Created by Alvaro Roman on 18/12/13.
-//  Copyright (c) 2013 Alvaro Roman. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
